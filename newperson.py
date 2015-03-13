b6b4d1f83a7955ac41eb1442c7d112db45703a7b
qoo <david@largitdata.com>
2015-03-13 12:05:20 +0800

    add person.py

---
+++ newperson.py
@@ -0,0 +1,8 @@
+from person import Person
+First = Person("David", "Chiayi") # Create a person with name as david, live in chiayi
+print First.number_of_persons() # 1
+
+Second = Person("QOO", "coca-cola")# Create a person with name as qoo, live in coca-cola
+print Second.number_of_persons() # 2
+
+print First.name
